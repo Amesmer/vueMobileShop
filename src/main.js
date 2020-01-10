c45b18772eaf2381e2331e4fa749bdb27c5907ba
Amesmer <535026442@qq.com>
2020-01-10 09:28:59 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/vant.js'
+// 导入axios
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+// 配置请求基地址
+axios.defaults.baseURL = 'http://www.liulongbin.top:3005/'
+
+Vue.prototype.$http = axios
 
 new Vue({
   router,
