275e9e548e6793be26d0d20861e0a7d958383b3c
Amesmer <535026442@qq.com>
2020-01-11 10:28:45 +0800

    wtf

---
+++ src/components/vip/vip.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+    <i class="set van-icon van-icon-setting-o"></i>
+    <p>正在加班加点开发中，敬请期待！</p>
+</div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="less" scoped>
+</style>
